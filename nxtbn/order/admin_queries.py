faf3069136fb5456af24db899651fdd03b69236e
MySecondLanguage <mmamunism@gmail.com>
2025-02-13 21:27:46 +0600

    Improved invoice query

--- nxtbn/order/admin_queries.py
+++ nxtbn/order/admin_queries.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 import graphene
 from graphene_django.filter import DjangoFilterConnectionField
 
 from nxtbn.core.admin_permissions import gql_store_admin_required
-from nxtbn.order.admin_types import OrderType
+from nxtbn.core.models import SiteSettings
+from nxtbn.order.admin_types import OrderInvoiceType, OrderType
 from nxtbn.order.models import Address, Order
 from nxtbn.users import UserRole
 
@@ -10,6 +12,8 @@ from nxtbn.users import UserRole
 class AdminOrderQuery(graphene.ObjectType):
     orders = DjangoFilterConnectionField(OrderType)
     order = graphene.Field(OrderType, id=graphene.Int(required=True))
+    order_invoice = graphene.Field(OrderInvoiceType, order_id=graphene.Int(required=True))
+    order_invoices = DjangoFilterConnectionField(OrderInvoiceType)
 
    
 
@@ -25,3 +29,22 @@ class AdminOrderQuery(graphene.ObjectType):
             raise Exception("Order not found")
         
         return order
+    
+   
+    @gql_store_admin_required
+    def resolve_order_invoice(self, info, order_id):
+        order = Order.objects.select_related(
+            'billing_address', 'shipping_address'
+        ).prefetch_related(
+            'line_items__variant'
+        ).get(id=order_id)
+
+        return order
+    
+    @gql_store_admin_required
+    def resolve_order_invoices(self, info, **kwargs):
+        order = Order.objects.select_related(
+            'billing_address', 'shipping_address'
+        ).prefetch_related(
+            'line_items__variant'
+        ).all()
--- nxtbn/order/admin_types.py
+++ nxtbn/order/admin_types.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 import graphene
 from graphene_django import DjangoObjectType
 from graphene import relay
-from nxtbn.order.models import Address, Order
+from nxtbn.core.models import SiteSettings
+from nxtbn.order.models import Address, Order, OrderLineItem
 
 from nxtbn.order.admin_filters import OrderFilter
 
@@ -40,6 +42,77 @@ class OrderType(DjangoObjectType):
             'created_at',
             'last_modified',
             'total_price',
+            'total_price_without_tax',
+            'total_shipping_cost',
+            'total_discounted_amount',
+            'total_tax',
+            'customer_currency',
+            'currency_conversion_rate',
+            'authorize_status',
+            'charge_status',
+            'promo_code',
+            'gift_card',
+            'payment_term',
+            'due_date',
+            'preferred_payment_method',
+            'reservation_status',
+            'note',
+            'comment',
+        )
+        interfaces = (relay.Node,)
+        filterset_class = OrderFilter
+
+
+
+class OrderInvoiceLineItemType(DjangoObjectType):
+    total_price = graphene.String()
+    price_per_unit = graphene.String()
+    name = graphene.String()
+
+    class Meta:
+        model = OrderLineItem
+        fields = ['id', 'quantity']
+
+    def resolve_total_price(self, info):
+        return self.humanize_total_price()
+
+    def resolve_price_per_unit(self, info):
+        return self.humanize_price_per_unit()
+
+    def resolve_name(self, info):
+        return self.variant.get_descriptive_name_minimal()
+
+class OrderInvoiceType(DjangoObjectType):
+    db_id = graphene.Int(source='id')
+    humanize_total_price = graphene.String()
+    items = graphene.List(OrderInvoiceLineItemType)
+    total_price = graphene.String()
+
+    def resolve_humanize_total_price(self, info):
+        return self.humanize_total_price()
+    
+    def resolve_company_info(self, info):
+        return SiteSettings.objects.first()
+
+    def resolve_items(self, info):
+        return self.line_items.all()
+
+    def resolve_total_price(self, info):
+        return self.humanize_total_price()
+
+    def resolve_payment_method(self, info):
+        return self.get_payment_method()
+    
+    class Meta:
+        model = Order
+        fields = (
+            'alias',
+            'id',
+            'status',
+            'shipping_address',
+            'billing_address',
+            'created_at',
+            'last_modified',
             'total_price_without_tax',
             'total_shipping_cost',
             'total_discounted_amount',
