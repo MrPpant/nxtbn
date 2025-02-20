66ac108df90dd75af84084f938aef461cf16da1b
MySecondLanguage <mmamunism@gmail.com>
2025-02-20 21:20:49 +0600

    Testcase written for order fullfillment

--- nxtbn/core/enum_perms.py
+++ nxtbn/core/enum_perms.py
@@ -9,6 +9,8 @@ class PermissionsEnum(models.TextChoices):
     CAN_DELIVER_ORDER = "can_deliver_order"
     CAN_UPDATE_ORDER_PYMENT_TERM = "can_update_order_payment_term"
     CAN_UPDATE_ORDER_PAYMENT_METHOD = "can_update_order_payment_method"
+    CAN_FULLFILL_ORDER = "can_fullfill_order" #  Directly fullfill order, will not check lifecycle
+    
 
     CAN_INITIATE_PAYMENT_REFUND = "CAN_INITIATE_PAYMENT_REFUND"
 
--- nxtbn/order/models.py
+++ nxtbn/order/models.py
@@ -258,6 +258,7 @@ class Order(MonetaryMixin, AbstractBaseUUIDModel):
             (PermissionsEnum.CAN_DELIVER_ORDER, 'Can deliver order'),
             (PermissionsEnum.CAN_UPDATE_ORDER_PYMENT_TERM, 'Can update order payment term'),
             (PermissionsEnum.CAN_UPDATE_ORDER_PAYMENT_METHOD, 'Can update order payment method'),
+            (PermissionsEnum.CAN_FULLFILL_ORDER, 'Can fullfill order'), # Directly fullfill order, will not check order lifecycle
         ]
 
     def save(self, *args, **kwargs):
